c0800d51cae5e4624d0ea42a8a59e6fad769a691
sgurung007 <sgrg007@gmail.com>
2021-12-23 21:14:22 -0600

    Add project files.

---
+++ cpp-app-1.cpp
@@ -0,0 +1,12 @@
+﻿// cpp-app-1.cpp : Defines the entry point for the application.
+//
+
+#include "cpp-app-1.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
